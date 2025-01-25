259adc088bee82e83f95076c36cff92d59481c8c
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-01-25 18:41:38 +0000

    Update run_signal_cls_1.py

--- run_signal_cls_1.py
+++ run_signal_cls_1.py
@@ -147,8 +147,8 @@ def parse_args():
     parser.add_argument(
         "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
     )
-    parser.add_argument("--output_dir", type=str, default=42, help="Where to store the final model.")
-    parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
+    parser.add_argument("--output_dir", type=str, default=None, help="Where to store the final model.")
+    parser.add_argument("--seed", type=int, default=42, help="A seed for reproducible training.")
     
     parser.add_argument("--push_to_hub", action="store_true", help="Whether or not to push the model to the Hub.")
     parser.add_argument(
