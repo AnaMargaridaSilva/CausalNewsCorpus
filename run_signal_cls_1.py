f970c0cd0380e462c4266a62ccf766cf428c51e0
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-01-28 11:39:30 +0000

    Update run_signal_cls_1.py

--- run_signal_cls_1.py
+++ run_signal_cls_1.py
@@ -284,14 +284,12 @@ def main():
         sentences = []
         labels = []    
         for i, text in enumerate(examples["text"]):
-            if len(eval(examples["causal_text_w_pairs"][i])) == 0:
-                continue
+            
+            sentences.append(text)
+            if any(['<SIG0>' in _ for _ in eval(examples["causal_text_w_pairs"][i])]):
+                labels.append(1)
             else:
-                sentences.append(text)
-                if any(['<SIG0>' in _ for _ in eval(examples["causal_text_w_pairs"][i])]):
-                    labels.append(1)
-                else:
-                    labels.append(0)
+                labels.append(0)
         return {"sentences": sentences, "labels": labels}
         
     raw_datasets = raw_datasets.map(preprocessing, batched=True, remove_columns=raw_datasets['train'].column_names)
